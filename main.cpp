3500383ab36f8e9484341e23e2d77b5af4a562a9
IvanCai335 <85745947+IvanCai335@users.noreply.github.com>
2023-03-20 16:50:19 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LibraryProject
+//
+//  Created by Ivan Cai on 2023-03-20.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
